15d6e945a8781a5525bfc1199777fb620b804a9d
ndeely <nialldeely@gmail.com>
2021-03-23 14:47:14 +0000

    added main activity and fragments
    - added dependencies
    - created menu package and fragments
    - setup main activity xml file
    - added primary color

--- app/src/main/java/com/example/aaachat/MainActivity.java
+++ app/src/main/java/com/example/aaachat/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.aaachat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.aaachat.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    private void setupViewPager(ViewPager vp) {
+
     }
 }
