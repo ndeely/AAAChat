e8a9604a7b7fa1dda53e248e59eca2b616195c59
ndeely <nialldeely@gmail.com>
2021-03-23 15:54:14 +0000

    Added main activity tabs
    - attached fragments to main activity using sectionsPagerAdapter
    - fixed app scrolling layout behaviour

--- app/src/main/java/com/example/aaachat/MainActivity.java
+++ app/src/main/java/com/example/aaachat/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.aaachat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.example.aaachat.databinding.ActivityMainBinding;
+import com.example.aaachat.menu.ChatsFragment;
+import com.example.aaachat.menu.ContactFragment;
+import com.example.aaachat.menu.StatusFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +27,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setupViewPager(binding.viewPager);
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
     }
 
     private void setupViewPager(ViewPager vp) {
+        MainActivity.SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
+        adapter.addFragment(new ChatsFragment(), "Chats");
+        adapter.addFragment(new StatusFragment(), "Status");
+        adapter.addFragment(new ContactFragment(), "Contact");
+
+        vp.setAdapter(adapter);
+    }
+
+    private static class SectionsPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
